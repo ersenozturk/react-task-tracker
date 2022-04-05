df728fcb5aa587609a81057f99bb694e6efbd532
ersen <70ozturkersen@gmail.com>
2022-04-05 17:22:28 +0300

    showAddTask and  toggleShow func added

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,15 +1,22 @@
 import Button from "./button/Button"
 
-const Header = ({title}) => {
+const Header = ({title, showAddTask, toggleShow }) => {
     
-    const handleClickUseforButtonComp = () => {
-        console.log('objbasadasdasect');
-      }
+    // const handleClickUseforButtonComp = () => {
+    //     console.log('objbasadasdasect');
+    //   }
 
   return (
     <div className="header">
         <h1>{title}</h1>
-        <Button handleClickUseforButtonComp={handleClickUseforButtonComp}  myText='Show Add Task Bar' myColor='#45858C'/>
+        
+        {/* <Button handleClickUseforButtonComp={handleClickUseforButtonComp}  myText='Show Add Task Bar' myColor='#45858C'/> */}
+
+        <Button
+        myColor={showAddTask ? "#F900BF" : "#4700D8"}
+        myText={showAddTask ? "Close Add Task Bar" : "Show Add Task Bar"}
+        toggleShow={toggleShow}
+      />
 
     </div>
   )
